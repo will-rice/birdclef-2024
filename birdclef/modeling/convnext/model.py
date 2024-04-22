a719ed4f67d97573511c3fb2d447c0499ac3ef0b
Will <wrice20@gmail.com>
2024-04-22 11:01:00 +0200

    spec only transforms

--- birdclef/modeling/convnext/model.py
+++ birdclef/modeling/convnext/model.py
@@ -12,7 +12,7 @@ from birdclef.signal import MelSpectrogram
 class ConvNextV2Classifier(nn.Module):
     """ConvNext model."""
 
-    def __init__(self, dropout: float = 0.2, num_classes: int = 182):
+    def __init__(self, dropout: float = 0.2, num_classes: int = 183):
         super().__init__()
         self.mel_fn = MelSpectrogram()
         self.normalize = v2.Normalize(IMAGENET_STANDARD_MEAN, IMAGENET_STANDARD_STD)
@@ -28,9 +28,6 @@ class ConvNextV2Classifier(nn.Module):
         """Forward pass."""
         if from_audio:
             x = self.mel_fn(x)
-        x = nn.functional.interpolate(
-            x, size=(self.image_size, self.image_size), mode="bilinear"
-        )
         x = self.normalize(x)
         x = self.encoder(x, return_dict=False)[1]
         x = self.dropout(x)
